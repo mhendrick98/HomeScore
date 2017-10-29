59c2e167e2396bead7241a99ed90fccf8f1f354f
Michael Hendrick <rsfmike@hotmail.com>
2017-10-28 21:18:59 -0400

    Config file
    Config

---
+++ Maps:Places:HubWayWork/config.js
@@ -0,0 +1,8 @@
+
+(function () {
+    "use strict";
+
+    exports.apiKey = process.env.GOOGLE_PLACES_API_KEY || "AIzaSyBWbWb-FA_nw22hOJOrDP8mZFakY2ggVME";
+    exports.outputFormat = process.env.GOOGLE_PLACES_OUTPUT_FORMAT || "json";
+
+})();
